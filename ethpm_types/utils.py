892b9e5d63fb6a13b08c1e0b7c3b992c57173a34
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2022-02-09 00:55:23 -0500

    feat: add sha3 and sha256 as supported checksum algos

--- ethpm_types/utils.py
+++ ethpm_types/utils.py
@@ -1,9 +1,11 @@
 from enum import Enum
-from hashlib import md5
+from hashlib import md5, sha3_256, sha256
 
 
 class Algorithm(str, Enum):
     MD5 = "md5"
+    SHA3 = "sha3"
+    SHA256 = "sha256"
 
 
 def is_valid_hash(data: str) -> bool:
@@ -18,10 +20,13 @@ def is_valid_hash(data: str) -> bool:
 
 def compute_checksum(content: bytes, algorithm: Algorithm = Algorithm.MD5) -> str:
     if algorithm is Algorithm.MD5:
-        return md5(content).hexdigest()
 
-    # TODO: Support sha3
-    # TODO: Support sha256
+    elif algorithm is Algorithm.SHA3:
+        return sha3_256(content).hexdigest()
+
+    elif algorithm is Algorithm.SHA256:
+        return sha256(content).hexdigest()
+
     # TODO: Support IPFS CIDv0 & CIDv1
     # TODO: Support keccak256 (if even necessary, mentioned in EIP but not used)
     # TODO: Explore other algorithms needed
