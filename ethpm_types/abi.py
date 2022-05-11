b47212d281f8063607cd58825409a8a9e6d76c41
unparalleled-js <yingthi@live.com>
2022-05-11 15:44:43 -0500

    fix: tuple check in abi type for getting canonical str

--- ethpm_types/abi.py
+++ ethpm_types/abi.py
@@ -21,7 +21,7 @@ class ABIType(BaseModel):
 
     @property
     def canonical_type(self) -> str:
-        if self.type == "tuple" and self.components:  # NOTE: 2nd condition just to satisfy mypy
+        if "tuple" in self.type and self.components:  # NOTE: 2nd condition just to satisfy mypy
             return f"({','.join(m.canonical_type for m in self.components)})"
 
         elif isinstance(self.type, str):
