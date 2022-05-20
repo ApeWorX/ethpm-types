614fcee4c6bf64b0a3c017be57ce1898d2fbeef0
unparalleled-js <yingthi@live.com>
2022-05-20 11:44:29 -0500

    chore: loosen eth-utils grip

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         "typing_extensions ; python_version<'3.8'",
         "hexbytes>=0.2.2,<0.3",
         "pydantic>=1.8.2,<2.0.0",
-        "eth-utils==1.10.0",
+        "eth-utils>=1.10.0,<3.0",
         "py-cid>=0.3.0,<0.4.0",
     ],
     python_requires=">=3.7.2,<3.11",
