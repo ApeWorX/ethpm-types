614a2fd10963d9e39484a463bd898aa1c6486642
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-06-02 14:59:34 -0400

    test: added test for hexbytes.HexBytes value

--- tests/test_hexbytes.py
+++ tests/test_hexbytes.py
@@ -1,3 +1,5 @@
+from hexbytes import HexBytes as OriginalHexBytes
+
 from ethpm_types import BaseModel, HexBytes
 
 TEST_MODEL_DICT = {
@@ -6,6 +8,7 @@ TEST_MODEL_DICT = {
     "str_bytes": "0x0",
     "int_bytes": 0,
     "bool_bytes": False,
+    "original_bytes": OriginalHexBytes("0"),
 }
 
 
@@ -15,6 +18,7 @@ class TestModel(BaseModel):
     str_bytes: HexBytes
     int_bytes: HexBytes
     bool_bytes: HexBytes
+    original_bytes: HexBytes
 
 
 def test_hexbytes_dict():
@@ -24,7 +28,7 @@ def test_hexbytes_dict():
         assert item.type_ == HexBytes
 
     test_dict = test_model.dict()
-    assert len(test_dict) == 5
+    assert len(test_dict) == 6
 
     for _, v in test_dict.items():
         assert v == "0x00"
@@ -34,7 +38,7 @@ def test_hexbytes_dict_exclude():
     test_model = TestModel.parse_obj(TEST_MODEL_DICT)
 
     test_dict = test_model.dict(exclude={"byte_bytes"})
-    assert len(test_dict) == 4
+    assert len(test_dict) == 5
     assert "byte_bytes" not in test_dict.keys()
 
 
@@ -42,4 +46,4 @@ def test_hexbytes_json():
     test_model = TestModel.parse_obj(TEST_MODEL_DICT)
     test_json = test_model.json(exclude={"bool_bytes", "byte_bytes", "int_bytes"})
 
-    assert test_json == '{"bytearray_bytes":"0x00","str_bytes":"0x00"}'
+    assert test_json == '{"bytearray_bytes":"0x00","original_bytes":"0x00","str_bytes":"0x00"}'
