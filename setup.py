ecb74b6e6dfd8c58325e7598387cf50070a2253c
unparalleled-js <yingthi@live.com>
2022-08-04 17:11:16 -0500

    chore: upgrades from template

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup  # type: ignore
 
 extras_require = {
     "test": [  # `test` GitHub Action jobs uses this
-        "pytest>=6.0,<7.0",  # Core testing package
+        "pytest>=6.0",  # Core testing package
         "pytest-xdist",  # multi-process runner
         "pytest-cov",  # Coverage analyzer plugin
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
@@ -14,14 +14,14 @@ extras_require = {
         "pysha3>=1.0.2,<2.0.0",  # Backend for eth-hash
     ],
     "lint": [
-        "black>=22.3.0,<23.0",  # auto-formatter and linter
-        "mypy>=0.950,<1.0",  # Static type analyzer
+        "black>=22.6.0",  # auto-formatter and linter
+        "mypy>=0.971",  # Static type analyzer
         "types-PyYAML",  # NOTE: Needed due to mypy typeshed
         "types-requests",  # NOTE: Needed due to mypy typeshed
-        "flake8>=3.9.2,<4.0",  # Style linter
-        "flake8-breakpoint>=1.1.0,<2.0.0",  # detect breakpoints left in code
-        "flake8-print>=4.0.0,<5.0.0",  # detect print statements left in code
-        "isort>=5.10.1,<6.0",  # Import sorting linter
+        "flake8>=<4.0.1",  # Style linter
+        "flake8-breakpoint>=1.1.0",  # detect breakpoints left in code
+        "flake8-print>=4.0.0",  # detect print statements left in code
+        "isort>=5.10.1",  # Import sorting linter
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
@@ -87,5 +87,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
