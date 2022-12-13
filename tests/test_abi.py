0bced30d4809ed1e5c9f16a2c2205e98b6b1933f
unparalleled-js <yingthi@live.com>
2022-12-13 15:24:31 -0600

    feat: ctor selector

---
+++ tests/test_abi.py
@@ -0,0 +1,9 @@
+from ethpm_types.abi import ABIType, ConstructorABI
+
+
+def test_constructor_selector():
+    constructor = ConstructorABI(
+        type="constructor",
+        inputs=[ABIType(name="contract_address", type="address", internalType="address")],
+    )
+    assert constructor.selector() == "constructor(address)"
