299a0dd1be385c44902e85405a894d1a7d175a87
Mike Shultz <shultzm@gmail.com>
2023-12-15 09:39:28 -0700

    fix: remove pysha3 from test dependencies (#105)

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ extras_require = {
         # Test-only deps
         "PyGithub>=1.54,<2.0",  # Necessary to pull official schema from github
         "hypothesis-jsonschema==0.19.0",  # Fuzzes based on a json schema
-        "pysha3>=1.0.2,<2.0.0",  # Backend for eth-hash
     ],
     "lint": [
         "black>=23.11.0,<24",  # Auto-formatter and linter
